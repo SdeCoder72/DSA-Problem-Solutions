8476c508915945556f1cc91fd70f75b08262fc94
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2025-11-11 22:57:01 +0530

    Implement tree traversal methods in binary tree
    Added traversal methods for pre-order, in-order, and post-order.

--- TreeDS/TreeImplementationUsingRecursion.java
+++ TreeDS/TreeImplementationUsingRecursion.java
@@ -3,7 +3,7 @@ package TreeDS;
 import java.util.Scanner;
 
 public class TreeImplementationUsingRecursion {
-    static class Node {
+    public static class Node {
         int data;
         Node left;
         Node right;
@@ -32,11 +32,46 @@ public class TreeImplementationUsingRecursion {
 
     }
 
+    // Traversal
+
+    public static void preOrder(Node root) {  // N L R
+        // base case
+        if(root == null) return;
+        // print Node
+        System.out.print(root.data + " ");
+        // go left
+        preOrder(root.left);
+        // go right
+        preOrder(root.right);
+    }
+    public static void inOrder(Node root) {  // L N R
+        if(root == null) return;
+        // go left
+        inOrder(root.left);
+        // print
+        System.out.print(root.data + " ");
+        // go right
+        inOrder(root.right);
+    }
+    public static void postOrder(Node root) {  // L R N
+        if(root == null) return;
+        // go left
+        postOrder(root.left);
+        // go right
+        postOrder(root.right);
+        // print
+        System.out.print(root.data + " ");
+    }
+
     public static void main(String[] args) {
         System.out.print("Enter the root Node: ");
         Node root = BinaryTree();
         System.out.println(root);
+        preOrder(root);
+        System.out.println();
+        inOrder(root);
+        System.out.println();
+        postOrder(root);
+        System.out.println();
     }
 }
-
-
