e2873b7c0848f6abb62ecbb3f904c4f6d4627328
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2025-12-28 23:45:40 +0530

    Add optimal approach for gas station problem
    Implemented an optimal approach for finding the starting gas station index. Added logic to calculate gas and cost sums and determine feasibility.

--- PracticeProblems_Week01/gasStation.java
+++ PracticeProblems_Week01/gasStation.java
@@ -1,3 +1,4 @@
+// Brute force approach (TLE)
 public int startStation(int[] gas, int[] cost) {
         // code here
         int n = gas.length;
@@ -17,4 +18,30 @@ public int startStation(int[] gas, int[] cost) {
         }
         
         return -1;
+    }
+
+// Optimal approach
+
+public int startStation(int[] gas, int[] cost) {
+        // code here
+        int n = gas.length;
+        int gasSum = 0;
+        int costSum = 0;
+        for(int i = 0; i<n; i++) {
+            gasSum += gas[i];
+        }
+        for(int i = 0; i<n; i++) {
+            costSum += cost[i];
+        }
+        if(costSum > gasSum) return -1;
+        int total = 0;
+        int idx = 0;
+        for(int i = 0; i<n; i++) {
+            total += gas[i] - cost[i];
+            if(total < 0) {
+                idx = i+1;
+                total = 0;
+            }
+        }
+        return idx;
     }
