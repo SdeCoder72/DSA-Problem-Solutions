fd72fdf8ae65299078da82b116e214e731fdfe47
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2026-01-09 11:08:46 +0530

    Implemented one Pass solution for SmallestSubtreeWithAllDeepestNodes.java

--- TreeDS/SmallestSubtreeWithAllDeepestNodes.java
+++ TreeDS/SmallestSubtreeWithAllDeepestNodes.java
@@ -23,3 +23,27 @@ class Solution {
         return lca(root);
     }
 }
+
+// 1 pass solution
+
+class Solution {
+    class Pair{
+        int depth;
+        TreeNode node;
+        Pair(int depth, TreeNode node) {
+            this.depth = depth;
+            this.node = node;
+        }
+    }
+    public Pair solve(TreeNode root) {
+        if(root == null) return new Pair(0, null);
+
+        Pair l = solve(root.left);
+        Pair r = solve(root.right);
+        if(l.depth == r.depth) return new Pair(l.depth + 1, root);
+        return (l.depth > r.depth)? new Pair(l.depth+1, l.node) : new Pair(r.depth+1, r.node);
+    }
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return solve(root).node;
+    }
+}
