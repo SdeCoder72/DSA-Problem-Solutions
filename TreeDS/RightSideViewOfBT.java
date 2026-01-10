5ac959a72a6fdf92b87874879225fdbd44c4f5db
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2026-01-10 07:19:37 +0530

    Implemented dfs traversal for RightSideViewOfBT.java

--- TreeDS/RightSideViewOfBT.java
+++ TreeDS/RightSideViewOfBT.java
@@ -1,4 +1,4 @@
-
+// BFS (level order) traversal
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
@@ -15,5 +15,22 @@ class Solution {
             }
         }
         return ans;
+    }
+}
+
+// DFS (pre-order) traversal
+
+class Solution {
+    void dfs(TreeNode root, List<Integer> ans, int lvl) {
+        if(root == null) return ;
+        if(ans.size() < lvl) ans.add(root.val);
+        dfs(root.right, ans, lvl+1);
+        dfs(root.left, ans, lvl+1);
+    }
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        dfs(root, ans, 1);
+        return ans;
+        
     }
 }
