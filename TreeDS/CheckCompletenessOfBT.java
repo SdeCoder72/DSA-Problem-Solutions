977ad706f3ddbe70478480e50059a1b1ec7912d8
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2026-01-12 10:24:52 +0530

    Implemented DFS to CheckCompletenessOfBT.java

--- TreeDS/CheckCompletenessOfBT.java
+++ TreeDS/CheckCompletenessOfBT.java
@@ -15,3 +15,22 @@ class Solution {
         return true;
     }
 }
+
+// DFS
+
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root == null) return 0;
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+    public boolean dfs(TreeNode root, int i, int totalNodes) {
+        if(root == null) return true;
+        if(i > totalNodes) return false;
+        return dfs(root.left, 2*i, totalNodes) && dfs(root.right, 2*i+1, totalNodes);
+    }
+    public boolean isCompleteTree(TreeNode root) {
+        int totalNodes = countNodes(root);
+        int i = 1;
+        return dfs(root, i, totalNodes);
+    }
+}
